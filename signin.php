ced98884d0187c09b795dcf78c6126d2abf1c848
Omar-Arja <omaralarja17@gmail.com>
2023-07-21 09:37:14 +0300

    fixed some issues

--- signin.php
+++ signin.php
@@ -6,13 +6,12 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 $query = $mysqli->prepare('select id, first_name, last_name, email, username, password
-from users 
-where username=?');
+from users');
 $query->bind_param('s', $username);
 $query->execute();
 
 $query->store_result();
-$query->bind_result($id, $username, $hashed_password, $first_name, $last_name);
+$query->bind_result($id, $first_name, $last_name, $email, $username, $hashed_password);
 $query->fetch();
 
 $num_rows = $query->num_rows();
@@ -24,6 +23,7 @@ if ($num_rows == 0) {
         $response['user_id'] = $id;
         $response['first_name'] = $first_name;
         $response['last_name'] = $last_name;
+        $response['email'] = $email;
         $response['username'] = $username;
     } else {
         $response['status'] = "wrong password";
